b50e0c2db94c1421a8ace07a7c8df07c471ba36a
Sahil Yeole <sahilyeole93@gmail.com>
2023-01-23 23:46:42 +0530

    added port to log

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use("*", (req, res) => {
 });
 const PORT = process.env.PORT || 4002;
 app.listen(PORT, () => {
-  console.log("Server is running on port 4000");
+  console.log(`Server is running on port ${PORT}`);
 });
